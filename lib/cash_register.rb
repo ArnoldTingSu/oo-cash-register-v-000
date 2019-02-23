001c8dcc623442b49ef8e18ceca3b2470912b984
Arnold Su <arnoldtingsu@gmail.com>
2019-02-23 06:43:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+##require 'pry'
 
+
+
+class CashRegister
+  attr_accessor :total, :items, :discount, :previous_total
+  
+
+
+def initialize(discount = 0)
+  @discount = discount
+  @total = 0
+  @items = []
+end
+
+def add_item(item, price, quantity = 1)
+  self.total += price * quantity
+  quantity.times do
+  @items << item
+  end
+  previous_total = price * quantity
+  
+ ## binding.pry
+end
+
+end
